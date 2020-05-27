a2a79ec2a9ede036e7184b666f1981b7691efa75
DevExpressExampleBot <fakeEmail>
2020-05-27 17:09:58 +0300

    Source auto update [skip ci]

---
+++ VB/CustomSeriesPointDrawingSample/Model/Order.vb
@@ -0,0 +1,25 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+Imports System
+
+Namespace CustomSeriesPointDrawingSample.Model
+
+	Partial Public Class Order
+		<Key>
+		<Required>
+		<Column("OrderID")>
+		Public Property OrderId() As Integer
+
+
+		<ForeignKey("Employee")>
+		<Column("EmployeeID")>
+		Public Property EmployeeId() As Integer?
+
+		Public Property OrderDate() As DateTime
+
+		<Column(TypeName := "smallmoney")>
+		Public Property Freight() As Decimal?
+
+		Public Overridable Property Employee() As Employee
+	End Class
+End Namespace
---
+++ VB/CustomSeriesPointDrawingSample/Program.vb
@@ -0,0 +1,21 @@
+﻿Imports DevExpress.XtraEditors
+Imports System
+Imports System.Windows.Forms
+
+Namespace CustomSeriesPointDrawingSample
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			WindowsFormsSettings.SetDPIAware()
+			WindowsFormsSettings.AllowDpiScale = True
+			WindowsFormsSettings.AllowAutoScale = DevExpress.Utils.DefaultBoolean.True
+
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
+End Namespace
